3d31ba4030f1ef5155ec5a1f628e8af1528a0670
joseantonio <joseantoniomartinez1904@gmail.com>
2021-11-29 11:56:32 +0100

    first commit

---
+++ src/main/java/com/everis/springConfig/SpringConfigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.everis.springConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/springConfig/SpringConfigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.springConfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
